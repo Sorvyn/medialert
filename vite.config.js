48d7bc003e2a7cca7e76364c16bf10f23b88c08e
Raja Sanaullah <rsanaullah566@gmail.com>
2026-06-06 18:51:44 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,4 @@
-.config · JS
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
- 
-export default defineConfig({
-  plugins: [react()]
-})
- 
+export default defineConfig({ plugins: [react()] })
+→ Commit ✅
